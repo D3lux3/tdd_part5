de94d797904a86f0bf4ba0d3084844c83e96800c
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2024-10-14 19:37:57 +0300

    Tests for returning all todos

--- backend/test/todoDb.test.ts
+++ backend/test/todoDb.test.ts
@@ -1,5 +1,5 @@
 import { Todo } from "../src/models";
-import { addNewTodo, deleteAllTodos } from "../src/services/todoService";
+import { addNewTodo, deleteAllTodos, getTodos } from "../src/services/todoService";
 
 
 describe('Todo database', () => {
@@ -28,5 +28,15 @@ describe('Todo database', () => {
     test('Invalid todo is not added', async () => {
         const invalidTodo = { done: false };
         await expect(addNewTodo(invalidTodo)).rejects.toThrow();
+    });
+
+    test('All todos are returned', async () => {
+        const newTodo = { name: 'Test todo', done: false };
+        const addedTodo = await addNewTodo(newTodo);
+        const todos = await getTodos();
+        expect(todos).toHaveLength(1);
+        expect(todos[0].name).toBe(addedTodo.name);
+        expect(todos[0].done).toBe(addedTodo.done);
+        expect(todos[0].id).toBe(addedTodo.id);
     });
 });
