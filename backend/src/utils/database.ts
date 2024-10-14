150d26fa3dd5ccb7114451fdca7748f948962cdf
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2024-10-14 20:05:59 +0300

    Disabled sequelize logging during testing

--- backend/src/utils/database.ts
+++ backend/src/utils/database.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const DATABASE_URL = process.env.DATABASE_URL || '';
 
 
-export const sequelize = new Sequelize(DATABASE_URL, { logging: (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') ? console.log : false});
+export const sequelize = new Sequelize(DATABASE_URL, { logging: (process.env.NODE_ENV === 'development') ? console.log : false});
 
 export const connectToDatabase = async () => {
     try {
