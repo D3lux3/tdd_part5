415f2d34124c97d0236349e1536637f968713bc0
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2024-10-21 16:52:22 +0300

    Added test for toggling done state of an todo

--- frontend/src/components/Todo/Todo.test.tsx
+++ frontend/src/components/Todo/Todo.test.tsx
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import '@testing-library/jest-dom';
 import Todo from './Todo';
 
 
+describe('Todo Component', () => {
 test('Renders Todo Component', () => {
     const todo = {
         id: '1',
@@ -13,4 +16,22 @@ test('Renders Todo Component', () => {
     render(<Todo {...todo} />);
     const element = screen.getByTestId(todo.id);
     expect(element).toHaveTextContent(todo.name);
+});
+
+test('Clicking checkbox calls handler once', async () => {
+    const todo = {
+        id: '1',
+        name: 'Test Todo',
+        done: false
+    }
+    const mockHandler = vi.fn();
+    
+    render(<Todo {...todo} toggleDone={mockHandler} />);
+    
+    const user = userEvent.setup();
+    const checkbox = screen.getByRole('checkbox');
+    await user.click(checkbox);
+
+    expect(mockHandler).toHaveBeenCalledTimes(1);
+});
 });
