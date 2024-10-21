f9090ab9e618e5c60ba21ba7bcd5d85ae69ff46c
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2024-10-21 17:26:59 +0300

    Added test for rendering todo listing

---
+++ frontend/src/components/TodoList/TodoList.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+describe("Todo List Component", () => {
+  test("Renders Todo List Component", () => {
+    const intitialTodos = [
+      { id: "1", name: "Test Todo 1", done: false },
+      { id: "2", name: "Test Todo 2", done: true },
+      { id: "3", name: "Test Todo 3", done: false },
+    ];
+
+    render(<TodoList todos={intitialTodos} />);
+    const todoListElement = screen.getByTestId("todo-list");
+    expect(todoListElement).toBeInTheDocument();
+  });
+});
