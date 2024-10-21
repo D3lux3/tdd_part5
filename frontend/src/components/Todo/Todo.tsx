af7481dfe759bcceafc063e084537e73b9f720f6
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2024-10-21 17:21:51 +0300

    Reformatted code

--- frontend/src/components/Todo/Todo.tsx
+++ frontend/src/components/Todo/Todo.tsx
@@ -1,40 +1,38 @@
 import { useState } from "react";
 
-
 interface TodoProps {
-    id: string,
-    name: string,
-    done: boolean,
-    toggleDone?: () => void
-    renameTodo?: () => void
-};
-
-const Todo = ({id, name, done, toggleDone, renameTodo}: TodoProps) => {
-    const [editing, setEditing] = useState(false);
-
-
-    if (editing) {
-        return (
-            <li data-testid={id}>
-            <form onSubmit={(e) => {
-                e.preventDefault();
-                renameTodo && renameTodo();
-                setEditing(false);
-            }}>
-                <input type="text" defaultValue={name} />
-                <button type="submit">Save</button>
-                </form>
-            </li>
-        )
-    }
-    return (
-        <li data-testid={id}>
-            <p onClick={() => setEditing(true)}>{name}</p>
-            <input type="checkbox" defaultChecked={done} name="done" onChange={() => toggleDone && toggleDone()}/>
-        </li>
-    )
+  id: string;
+  name: string;
+  done: boolean;
+  toggleDone?: () => void;
+  renameTodo?: () => void;
 }
 
+const Todo = ({ id, name, done, toggleDone, renameTodo }: TodoProps) => {
+  const [editing, setEditing] = useState(false);
 
+  if (editing) {
+    return (
+      <li data-testid={id}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            renameTodo && renameTodo();
+            setEditing(false);
+          }}
+        >
+          <input type="text" defaultValue={name} />
+          <button type="submit">Save</button>
+        </form>
+      </li>
+    );
+  }
+  return (
+    <li data-testid={id}>
+      <p onClick={() => setEditing(true)}>{name}</p>
+      <input type="checkbox" defaultChecked={done} name="done" onChange={() => toggleDone && toggleDone()} />
+    </li>
+  );
+};
 
 export default Todo;
