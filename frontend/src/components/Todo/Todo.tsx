3d91283a4fb104deb0c50b60890ebe59a46e6f73
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2024-10-21 16:56:22 +0300

    Added checkbox to Todo component

--- frontend/src/components/Todo/Todo.tsx
+++ frontend/src/components/Todo/Todo.tsx
@@ -4,14 +4,16 @@ interface TodoProps {
     id: string,
     name: string,
     done: boolean,
+    toggleDone?: () => void
 };
 
-const Todo = ({id, name, done}: TodoProps) => {
+const Todo = ({id, name, done, toggleDone}: TodoProps) => {
 
 
     return (
         <li data-testid={id}>
             {name}
+            <input type="checkbox" defaultChecked={done} value="done" onChange={() => toggleDone && toggleDone()}/>
         </li>
     )
 }
