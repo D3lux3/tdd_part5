5b9313b10b237054581601f867e7fc32dc59ee6b
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2024-10-21 17:07:41 +0300

    Reformatted

--- frontend/src/components/Todo/Todo.test.tsx
+++ frontend/src/components/Todo/Todo.test.tsx
@@ -1,40 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import '@testing-library/jest-dom';
-import Todo from './Todo';
+import "@testing-library/jest-dom";
+import Todo from "./Todo";
 
-
-describe('Todo Component', () => {
-test('Renders Todo Component', () => {
+describe("Todo Component", () => {
+  test("Renders Todo Component", () => {
     const todo = {
-        id: '1',
-        name: 'Test Todo',
-        done: false
-    }
+      id: "1",
+      name: "Test Todo",
+      done: false,
+    };
 
     render(<Todo {...todo} />);
     const element = screen.getByTestId(todo.id);
     expect(element).toHaveTextContent(todo.name);
-});
+  });
 
-test('Checkbox click changes done state', async () => {
+  test("Checkbox click changes done state", async () => {
     const todo = {
-        id: '1',
-        name: 'Test Todo',
-        done: false
-    }
+      id: "1",
+      name: "Test Todo",
+      done: false,
+    };
     const mockHandler = vi.fn();
-    
+
     render(<Todo {...todo} toggleDone={mockHandler} />);
-    
+
     const user = userEvent.setup();
-    const checkbox = screen.getByRole('checkbox');
+    const checkbox = screen.getByRole("checkbox");
 
     expect(checkbox).not.toBeChecked();
     await user.click(checkbox);
 
     expect(checkbox).toBeChecked();
     expect(mockHandler).toHaveBeenCalledTimes(1);
-});
+  });
 });
