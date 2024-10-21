6bfb31c739ada52eb0ba1826ed2ccbfdb63424e6
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2024-10-21 20:15:37 +0300

    Added new todo adding logic

--- frontend/src/components/TodoList/Todolist.tsx
+++ frontend/src/components/TodoList/Todolist.tsx
@@ -1,15 +1,51 @@
-import { TodoType } from "../../types";
+import React, { useState } from "react";
+import { TodoType, TodoWithoutId } from "../../types";
 import Todo from "../Todo/Todo";
 
 interface TodoListProps {
   todos: TodoType[];
   archiveTodoHandler?: () => void;
+  createTodo?: (newTodo: TodoWithoutId) => void;
 }
 
-const TodoList = ({ todos, archiveTodoHandler }: TodoListProps) => {
+const TodoList = ({ todos, archiveTodoHandler, createTodo }: TodoListProps) => {
+  const [todoInputValue, setTodoInputValue] = useState<string>("");
+
+  const createTodoHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const newTodo = {
+        name: todoInputValue,
+        done: false,
+    }
+    if (createTodo) {
+      setTodoInputValue("");
+      return createTodo(newTodo);
+    }
+    try {
+        // Add new todo functionality
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
-      <button data-testid="todo-archive-all" onClick={archiveTodoHandler}>Archive Done Todos</button>
+      <form onSubmit={createTodoHandler}>
+        <a>Todo name</a>
+        <input
+          data-testid="new-todo-input"
+          type="text"
+          value={todoInputValue}
+          onChange={(event) => setTodoInputValue(event.target.value)}
+          name="name"
+        />
+        <button data-testid="new-todo-save" type="submit">
+          Add new
+        </button>
+      </form>
+
+      <button data-testid="todo-archive-all" onClick={archiveTodoHandler}>
+        Archive Done Todos
+      </button>
       <ul data-testid="todo-list">
         {todos.map((todo) => (
           <Todo key={todo.id} {...todo} />
