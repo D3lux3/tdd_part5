2273113b478054178bc0c0ad972c369ba416c144
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2024-10-21 17:29:00 +0300

    Added Todolist import

--- frontend/src/components/TodoList/TodoList.test.tsx
+++ frontend/src/components/TodoList/TodoList.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import TodoList from "./Todolist";
 
 describe("Todo List Component", () => {
   test("Renders Todo List Component", () => {
